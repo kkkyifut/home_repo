5f0679281d8cd7c3e909ea297114172d62df8928
Юрий <ura.yakovlev@yandex.ru>
2021-03-25 21:50:53 +0300

    Коммит

--- samplesite/samplesite/settings.py
+++ samplesite/samplesite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # наши приложения
+    'bboard.apps.BboardConfig',
 ]
 
 MIDDLEWARE = [
